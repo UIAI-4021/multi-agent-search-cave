f43ee793c295c4cdeac40115a65bacaf139316db
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2024-01-08 10:51:47 +0330

    now it prefers lonly points

--- multiAgents.py
+++ multiAgents.py
@@ -16,6 +16,7 @@ from util import manhattanDistance
 from game import Directions
 import random, util
 import math
+import time
 import random
 from game import Agent
 from pacman import GameState
@@ -39,22 +40,8 @@ class MultiAgentSearchAgent(Agent):
         self.time_limit = int(time_limit)
 class AIAgent(MultiAgentSearchAgent):
     def getAction(self, gameState: GameState):
-        def greed(action, bestAction, EF):
-            BV3 = minimax_value(gameState.generateSuccessor(0, bestAction), 1, 3, -20000, 20000)
-            V3 = minimax_value(gameState.generateSuccessor(0, action), 1, 3, -20000, 20000)
-            V4 = minimax_value(gameState.generateSuccessor(0, action), 1, 4, -20000, 20000) - 5
-            BV4 = minimax_value(gameState.generateSuccessor(0, action), 1, 4, -20000, 20000) - 5
-            EF -= 2
-            print(V3, BV3, EF, V4, BV4)
-            if BV3 + EF == 2 * V3 and V4 > EF:
-                print('111111111111111111111111111111')
-                return 1
-            if V3 + EF == 2 * BV3 and BV4 > EF:
-                print('222222222222222222222222222222')
-                return 2
-            return 0
-
         def minimax_decision(gameState):
+            Start = time.time()
             legalActions = gameState.getLegalActions(0)
             legalActions.remove('Stop')
             bestAction = None
@@ -62,36 +49,47 @@ class AIAgent(MultiAgentSearchAgent):
             worst = math.inf
             EF = self.evaluationFunction(gameState)
             q = True
+            v = [4, 1]
+            bv = [4, 1]
             for action in legalActions:
+                v[0] = minimax_value(gameState.generateSuccessor(0, action), 1, 3, bestValue, worst)
+                v[1] = minimax_value(gameState.generateSuccessor(0, action), 1, 4, bestValue, worst)
+                if(v[0] - EF == 8 and v[1] - EF == 9):
+                    bestAction = action
+                    q = False
+                    break
                 value = minimax_value(gameState.generateSuccessor(0, action), 1, 0, bestValue, worst)
                 if value > EF:
                     q = False
+                    
                 print(action, value)
+
                 for i in range(5):
                     if i:
-                        V = minimax_value(gameState.generateSuccessor(0, action), 1, i, bestValue, worst)
-                        BV = minimax_value(gameState.generateSuccessor(0, bestAction), 1, i, bestValue, worst)
-                        print(action, V, bestAction, BV)
+                        if i > 2:
+                            V = v[i - 3]
+                            BV = bv[i - 3]
+                        else:
+                            V = minimax_value(gameState.generateSuccessor(0, action), 1, i, bestValue, worst)
+                            BV = minimax_value(gameState.generateSuccessor(0, bestAction), 1, i, bestValue, worst)
+                            print(action, V, bestAction, BV)
                     else:
                         V = value
                         BV = bestValue
+
                     if V != BV:
                         if V > BV:
                             print('Changed')
                             bestValue = value
+                            bv[0] = v[0]
+                            bv[1] = v[1]
                             bestAction = action
                         break
-                # if bestAction != None:
-                #     g = greed(action, bestAction, EF)
-                #     if g > 0:
-                #         if g == 1:
-                #             bestValue = value
-                #             bestAction = action
-                #         break
             if q:
                 print('random')
                 bestAction = random.choice(legalActions)
             print(bestAction)
+            print(time.time() - Start)
             return bestAction
 
         def minimax_value(gameState, agentIndex, depth, bestsofar, worst):
